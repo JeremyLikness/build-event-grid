686018ce02532b7a23507f5a6928f7795614f111
Jeremy Likness <jeremy@jeremylikness.com>
2018-04-30 15:58:50 -0400

    Initial build sample project

---
+++ broadcaster/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace publisher
+{
+    class Program
+    {
+        private static readonly string Key = Environment.GetEnvironmentVariable("EVENT_GRID_KEY");
+        private static readonly string Endpoint = Environment.GetEnvironmentVariable("EVENT_GRID_URL");
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) 
+            {
+                Console.WriteLine("Pass the message to send.");
+                return;
+            }
+            SendEvent(args[0]).Wait();
+            Console.WriteLine("Successfully published.");
+        }
+
+        private static async Task SendEvent(string msg)
+        {
+            List<object> events = new List<object>();
+            dynamic payload = new JObject();
+            payload.Id = Guid.NewGuid().ToString();
+            payload.EventType = "BuildMessage";
+            payload.Subject = msg;
+            payload.EventTime = DateTimeOffset.Now.ToString("o");
+            payload.Data = new JObject();
+            payload.Data.message = msg;
+            events.Add(payload);
+            var content = JsonConvert.SerializeObject(events);
+            Console.WriteLine(content);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("aeg-sas-key", Key);
+            var httpContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var result = await client.PostAsync(Endpoint, httpContent);
+            var resultText = await result.Content.ReadAsStringAsync();
+            Console.WriteLine($"Response: {result.StatusCode} - {resultText}.");
+        }
+    }
+}
---
+++ receiver/Controllers/BuildController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Newtonsoft.Json;
+
+namespace receiver.Controllers
+{
+    [Route("api/[controller]")]
+    public class BuildController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]EventGridEvent[] value)
+        {
+            if (value == null || value.Length != 1 || value[0].Data == null)
+            {
+                Console.WriteLine("Bad event.");
+                return BadRequest();
+            }
+
+            Console.WriteLine($"Received event with type {value[0].EventType}");
+
+            if (value[0].EventType == "Microsoft.EventGrid.SubscriptionValidationEvent")
+            {
+                var subscriptionValidation = value[0].Data.ToObject<SubscriptionValidationEvent>();
+                Console.WriteLine($"Validating subscription with token {subscriptionValidation.ValidationCode}");
+                
+                return Ok(new SubscriptionValidationResponse
+                {
+                    ValidationResponse = subscriptionValidation.ValidationCode   
+                });
+            }
+
+            Console.WriteLine($"Parsing payload {value[0].Data}...");
+            var msg = value[0].Data["message"].ToString();
+            Console.WriteLine($"Received message: {msg}");
+
+            return Ok();
+        }
+    }
+}
